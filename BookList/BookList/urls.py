06b48743b32a64095be1965f42841c6d2d4585c0
Iskander Ismagilov <kismagilov135@gmail.com>
2025-10-28 15:24:28 +0300

    design changes

--- BookList/BookList/urls.py
+++ BookList/BookList/urls.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e8fe850b1feb51ecfa0ee627c3e836fc0f7b7865c4b267f3fc8e925f2dc1069
-size 1141
+oid sha256:e3809202f882fcd43775c0b2aa51c4b3b5aa6e21f37ccabc05b3caeb2c942704
+size 1225
--- BookList/firstapp/views.py
+++ BookList/firstapp/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b011ee69c0de72ec2c4ce8261505be8540e4047fabe5b08b448f8e3abbf6c25
-size 18413
+oid sha256:9b5bf650879200988ab119b1759c8c015c9f19ca16be97ce746c02b13c7195cb
+size 18771
